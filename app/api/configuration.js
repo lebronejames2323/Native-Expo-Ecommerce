f7b8c15bfab2bda465aed4912f3a4f74fc6a25f0
lebronejames2323 <ginovillar02gmail.com>
2025-08-01 18:46:35 +0800

    changing from railway to render backend

--- app/api/configuration.js
+++ app/api/configuration.js
@@ -1,7 +1,7 @@
-export const url = "https://backendlatest-production.up.railway.app/api";
-export const imageUrl1 = "https://backendlatest-production.up.railway.app/storage/uploads/products";
-export const imageUrl2 = "https://backendlatest-production.up.railway.app/storage/uploads/categories";
-export const imageUrl3 = "https://backendlatest-production.up.railway.app/storage/uploads/reviews";
+export const url = "https://backend-latest-hke7.onrender.com/api";
+export const imageUrl1 = "https://backend-latest-hke7.onrender.com/storage/uploads/products";
+export const imageUrl2 = "https://backend-latest-hke7.onrender.com/storage/uploads/categories";
+export const imageUrl3 = "https://backend-latest-hke7.onrender.com/storage/uploads/reviews";
 
 export default {
     url,
